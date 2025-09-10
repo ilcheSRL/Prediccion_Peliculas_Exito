db423fb399d14588bfc120f0d319fcbc291d8bc3
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-09 22:59:08 -0500

    Update Step_01_Cleanning.py

--- Logic/Step_01_Cleanning.py
+++ Logic/Step_01_Cleanning.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+class IntegrationTransformer:
+    def Cleaning_Data(self, df: pd.DataFrame, preview_rows: int = 5) -> pd.DataFrame:
+        """
+        Punto inicial de integración.
+        Por ahora, solo muestra un preview (display) y retorna el DataFrame sin cambios.
+        """
+        df = df[df["periodo"] > 202301]
+
+
+      
+        return df
