9a1224348cd9f883dd8e8a66311fed3d486123c9
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-09 22:55:37 -0500

    Update constante.py

--- Recursos/constante.py
+++ Recursos/constante.py
@@ -6,6 +6,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Rutas a los archivos CSV de entrada
-RESOURCES_DIR = os.path.join(BASE_DIR, 'Recursos')
+RESOURCES_DIR = os.path.join(BASE_DIR, '')
 Data_N1 = os.path.join(RESOURCES_DIR, 'master_df.csv')
  
