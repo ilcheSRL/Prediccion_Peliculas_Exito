20cc9669b85708d34ff7aa00c41c5117a733b0c3
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-09 21:58:32 -0500

    Update 00_Integracion.py

--- Logic/00_Integracion.py
+++ Logic/00_Integracion.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+class IntegrationTransformer:
+    def integrate_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Punto inicial de integración de datos.
+        Por ahora, solo devuelve el DataFrame recibido.
+        
+        Args:
+            df (pd.DataFrame): DataFrame de entrada.
+        Returns:
+            pd.DataFrame: DataFrame de salida (sin cambios).
+        """
+
+
+      
+        return df
