031c37437f62757015c6b5a47452a29e6d7e27a6
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-09 22:46:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 from utils.utils import Utils
 from business_logic.integration import IntegrationTransformer
 from constante import (Data_N1)
-
-utils/00_utils 
+ 
 
 def run_pipeline():
     # Paso 1: Lectura de datos con Utils
