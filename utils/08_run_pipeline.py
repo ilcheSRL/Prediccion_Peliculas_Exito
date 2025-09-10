1262e17abaaa52a24e4f78a0ff66c58c35465a2f
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-09 22:10:09 -0500

    Update 08_run_pipeline.py

--- utils/08_run_pipeline.py
+++ utils/08_run_pipeline.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from Logic._00_Integracion import IntegrationTransformer
+from project.constants import Data_N1  # importar la constante con la ruta
+
+def run_pipeline():
+    # Leer el DataFrame desde constants.py
+    df = pd.read_csv(Data_N1)
+
+    # Paso 1: Integración
+    df = IntegrationTransformer().integrate_data(df)
+
+    # Mostrar resultado
+    print("=== DataFrame Integrado ===")
+    print(df.head())  # primeras filas
+
+if __name__ == "__main__":
+    run_pipeline()
 
