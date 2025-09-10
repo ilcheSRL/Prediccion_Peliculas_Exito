4feb3f23f5580b416cd425c988ea69604d632b4f
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-09 23:07:27 -0500

    Update and rename 02_Feature_Engenierring.py to Step_02_Feature_Engenierring.py

---
+++ Logic/Step_02_Feature_Engenierring.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+class FeatureEngenieringTransformer:
+    def Feature_Step_001_Data(self, df: pd.DataFrame, preview_rows: int = 5) -> pd.DataFrame:
+        """
+        Punto inicial de integración.
+        Por ahora, solo muestra un preview (display) y retorna el DataFrame sin cambios.
+        """
+        df["periodo"] = pd.to_numeric(df["periodo"], errors="coerce")
+        df = df[df["target"] == 1]
+        print(df)
+
+      
+        return df
