a82d79c4822a99d4263d91d986673ff36be642f7
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-10 21:04:33 -0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -2,14 +2,12 @@ import pandas as pd
 
 class Utils:
     @staticmethod
-    def load_table(path: str) -> pd.DataFrame:
- 
-            df = pd.read_csv(path)
- 
+    def load_table(path: str) -> pd.DataFrame: 
+            df = pd.read_csv(path) 
             return df
-       
+
+
+    
     def load_table_csv(path: str) -> pd.DataFrame:
- 
-            df = pd.read_csv(path, header=0, sep=",")
- 
+            df = pd.read_csv(path, sep=",", encoding="latin1") 
             return df
