756a1e48bb270a6cc20226c741f886557e09e681
Ilche Park Data Explore <48047249+ilcheSRL@users.noreply.github.com>
2025-09-10 21:00:29 -0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -10,6 +10,6 @@ class Utils:
        
     def load_table_csv(path: str) -> pd.DataFrame:
  
-            df = pd.read_csv(path, header = True, Sep = ",")
+            df = pd.read_csv(path, header=0, sep=",")
  
             return df
